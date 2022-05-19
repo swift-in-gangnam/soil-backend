188be44ea6e42216a440efa5ae1ffabb13a4dcdc
sksk713 <sskk713@ajou.ac.kr>
2022-05-19 18:44:47 +0900

    feat: Project 생성

---
+++ src/main/java/com/swift/soil/SoilApplication.java
@@ -0,0 +1,13 @@
+package com.swift.soil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoilApplication.class, args);
+	}
+
+}
